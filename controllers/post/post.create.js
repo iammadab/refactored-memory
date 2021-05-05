11a4e94082e00abcca1e306780fca6826437676b
iammadab <iammadab@gmail.com>
2021-05-05 08:34:00 +0000

    Creating post submission with optional image upload

--- controllers/post/post.create.js
+++ controllers/post/post.create.js
@@ -1,6 +1,39 @@
-const postService = require("../services/post.service")
+const postService = require("../../services/post")
+const fileService = require("../../services/file")
+
+const joi = require("joi")
+
+const createPostValidator = joi.object({
+  body: joi.string().trim().required(),
+  images: joi.array().items(joi.string()),
+  user: joi.object({
+    _id: joi.object().required()
+  }).required().unknown(true)
+}).options({ abortEarly: false }).unknown(true)
 
 const createPost = async (data) => {
+  
+  const validationResult = createPostValidator.validate(data)
+  if(validationResult.error)
+    return { status: 400, code: "BAD_REQUEST_ERROR", errors: validationResult.error }
+
+  data = validationResult.value
+
+  // Verify that all the files exists
+  const files = await fileService.findFiles(data.images)
+  if(files.error)
+    return { status: 404, code: "COULD_NOT_FIND_IMAGES" }
+
+  const post = await postService.createPost({
+    user: data.user._id,
+    body: data.body,
+    images: files.map(file => file._id)
+  })
+
+  if(!post)
+    return { status: 500, code: "ERROR_CREATING_POST" }
+
+  return { status: 200, code: "CREATED_POST_SUCCESSFULLY", data: post }
 
 }
 
--- services/file.js
+++ services/file.js
@@ -8,3 +8,11 @@ exports.createFile = async (data) => {
     return { error: true, code: "ERROR_CREATING_FILE" }
   }
 }
+
+exports.findFiles = async (ids) => {
+  try {
+    return await File.find({ _id: { $in: ids } })
+  } catch (error){
+    return { error: true, code: "ERROR_FINDING_FILES" }
+  }
+}
